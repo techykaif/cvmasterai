bab3a18b1192978ffd43fc77307d9c9f0a07296f
techykaif <mokaifansari786@gmail.com>
2025-02-20 19:34:22 +0530

    updated codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "AI-Powered Resume Builder",
-  description: "Create, customize, and optimize your resume effortlessly using AI.",
-<<<<<<< HEAD
-    generator: 'v0.dev'
-=======
->>>>>>> 21d9fab (updated footer)
+  description: "Create, customize, and optimize your resume effortlessly using AI."
 }
 
 export default function RootLayout({
